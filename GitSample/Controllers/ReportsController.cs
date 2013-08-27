45f3cd7b848462ade199803ccf4ec4c9028121f1
kljanicki <kjanicki@anexinet.com>
2013-08-27 18:03:34 -0400

    new controller

---
+++ GitSample/Controllers/ReportsController.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GitSample.Controllers
+{
+    public class ReportsController : Controller
+    {
+        //
+        // GET: /Reports/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
